c2a27f0a300b1dd163e122844bbce766d0f8f7f4
Roman Babichev <rbabichev@fortinet-us.com>
2020-09-30 03:12:29 -0700

    adding ip input from serial in progress

--- src/Web_Authentication.ino
+++ src/Web_Authentication.ino
@@ -9,14 +9,14 @@
 // Init outlet relay pins and state
 //-------------------------------------------------------------------------------------------------------
 String readString;
+char inputChar;
 char buf[128];
 int relayPins[] = {2, 3, 4, 6};
-// int pc1 = 0;
-// int pc2 = 0;
-// int fgt1 = 0;
-// int fgt2 = 0;
-
-int outletStates[] = {1, 1, 1, 1};
+int outletStates[] = {0, 0, 0, 0};
+static uint8_t mac[] = {0xF9, 0x62, 0x30, 0x4C, 0x7D, 0x5C};
+// static uint8_t ip[] = {192, 168, 1, 210}; /* ip is set dinamically through serial */
+IPAddress ipAddr(192, 168, 1, 210);
+bool editing = false;
 
 P(pageStart) = "<!DOCTYPE html><html><head>"
           "<title>Webduino AJAX RGB Example</title>"
@@ -82,14 +82,16 @@ P(pageStart) = "<!DOCTYPE html><html><head>"
           "</style>"
           "</head>"
           "<body>";
+
+/* store common switch html code parts on onboard memory to free up RAM */
 P(switchHtmlStart) = "<div class=\"onoffswitch\">"
                       "<input type=\"checkbox\" name=\"onoffswitch\" class=\"onoffswitch-checkbox\" ";
 P(switchHtmlEnd) = "<span class=\"onoffswitch-inner\"></span><span class=\"onoffswitch-switch\"></span>"
                     "</label></div>";
 P(pageEnd) = "</body></html>";
 
+/* get string with html for (un)checked switch based on relay state */
 char* HtmlSwitchChecked(char* res, int outletIndex) {
-  // char res[128];
   char name[5];
   switch (outletIndex) {
     case 0:
@@ -111,19 +113,44 @@ char* HtmlSwitchChecked(char* res, int outletIndex) {
   Serial.println(res);
   return res;
 }
-/* CHANGE THIS TO YOUR OWN UNIQUE VALUE.  The MAC number should be
- * different from any other devices on your network or you'll have
- * problems receiving packets. */
-static uint8_t mac[] = {0xF9, 0x62, 0x30, 0x4C, 0x7D, 0x5C};
 
-/* CHANGE THIS TO MATCH YOUR HOST NETWORK.  Most home networks are in
- * the 192.168.0.XXX or 192.168.1.XXX subrange.  Pick an address
- * that's not in use and isn't going to be automatically allocated by
- * DHCP from your router. */
-static uint8_t ip[] = {192, 168, 1, 210};
+/* set ip dynamically */
+int readCommands() {
+  if (Serial.available() > 0) {
+    inputChar = Serial.read();
+    editing = true;
+    
+    Serial.println("help or setip");
+    delay(3000);
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+    Serial.print(input.length());
+    Serial.println(input);
+    
+    if (input == "help" || input == "h") {
+      Serial.println("setip to set ip");
+    } else if (input == "setip") {
+      delay(3000);
+      input = Serial.readStringUntil('\n');
+      input.trim();
+      if (input.length() == 16) {
+        ipAddr.fromString(input);
+      } else {
+        Serial.println("Wrong ip");
+        return -1;
+      }
+      Serial.println(ipAddr);
+      Ethernet.begin(mac, ipAddr);
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+  return -1;
+}
 
 /* This creates an instance of the webserver.  By specifying a prefix
- * of "", all pages will be at the root of the server. */
+ * of(r?) "", all pages will be at the root of the server. */
 #define PREFIX ""
 WebServer webserver(PREFIX, 80);
 
@@ -140,16 +167,16 @@ void relayCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) {
       Serial.println(numRes);
       if (strcmp(relayName, "pc1") == 0) {
         outletStates[0] = numRes;
-        digitalWrite(relayPins[0], outletStates[0] == 0 ? HIGH : LOW);
+        digitalWrite(relayPins[0], outletStates[0] == 1 ? HIGH : LOW);
       } else if (strcmp(relayName, "pc2") == 0) {
         outletStates[1] = numRes;
-        digitalWrite(relayPins[1], outletStates[1] == 0 ? HIGH : LOW);
+        digitalWrite(relayPins[1], outletStates[1] == 1 ? HIGH : LOW);
       } else if (strcmp(relayName, "fgt1") == 0) {
         outletStates[2] = numRes;
-        digitalWrite(relayPins[2], outletStates[2] == 0 ? HIGH : LOW);
+        digitalWrite(relayPins[2], outletStates[2] == 1 ? HIGH : LOW);
       } else if (strcmp(relayName, "fgt2") == 0) {
         outletStates[3] = numRes;
-        digitalWrite(relayPins[3], outletStates[3] == 0 ? HIGH : LOW);
+        digitalWrite(relayPins[3], outletStates[3] == 1 ? HIGH : LOW);
       }
     }
     server.httpSeeOther(PREFIX);
@@ -196,7 +223,7 @@ void relayCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) {
     //-------------------------------------------------------------------------------------------------------
     // Init webserver
     //-------------------------------------------------------------------------------------------------------
-    Ethernet.begin(mac, ip);
+    Ethernet.begin(mac, ipAddr);
     webserver.setDefaultCommand(&relayCmd);
     webserver.begin();
 
@@ -218,12 +245,12 @@ void relayCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) {
 
     Serial.print("Server is at ");
     Serial.println(Ethernet.localIP());
-    Serial.println("LED Controller Test 1.0");
   }
 
   void loop() {
-    // char buff[64];
-    // int len = 64;
+    // Serial.println("loop");
+    int res = readCommands();
+    // Serial.println(res);
 
     /* process incoming connections one at a time forever */
     webserver.processConnection();
