ad0eff0a56ba132fa5fa8651d8e5342667ab365e
hy6p <roman.tata.b@gmail.com>
2021-02-16 03:01:51 -0800

    Custom AES library. Padding is invalid on server side.

--- src/main.ino
+++ src/main.ino
@@ -6,7 +6,7 @@
 // #include "ArduinoJson.h"
 #include "millisDelay.h"
 #include <SPI.h>
-#include "Crypto.h"
+// #include "Crypto.h"
 #include "AES.h"
 // #include "sercerts.h"
 
@@ -34,7 +34,7 @@ EthernetServer swServer(1234);
 byte updSrvAddr[] = {192, 168, 0, 114};
 int srvPort = 7071;
 
-AESSmall128 aes128;
+AES aes128;
 
 void encrypt() {
 
@@ -44,21 +44,25 @@ void encrypt() {
 void sendUpdate(IPAddress ip) {
 	// webserver.reset();
 	// IPAddress *localip;
+	memset(buf, 0, sizeof(buf));
 	sprintf(buf, "{\"password\": \"secpass123\",\"name\": \"ard\",\"payload\": {\"deviceName\": \"arduino\",\"ipAddress\": \"%d.%d.%d.%d\",}}\0", ip[0], ip[1], ip[2], ip[3]);
 
 	const uint8_t *key = "abcdefghijklmnop";
-	String test = "abcd";
-	uint8_t *test_input;
-	test_input = (uint8_t *)malloc(sizeof(test.c_str()));
+	byte iv[N_BLOCK];
+	memcpy(iv, key, N_BLOCK);
+	// String test = "abcd";
+	// uint8_t *test_input;
+	// test_input = (uint8_t *)malloc(sizeof(test.c_str()));
 	// memset(test_input, 1, sizeof(test_input));
 	// snprintf((char *)test_input, sizeof(test_input), test);
 	// byte test_buf[128];
-	aes128.setKey(key, 16);
+	aes128.clean();
+	aes128.set_key((byte *)key, 16);
 	// aes128.encryptBlock(test_buf, (uint8_t *)test.c_str());
 	// aes128.~BlockCipher
 	// Serial.println(test);
 	// printArr(test_buf);
-	Serial.println("...encrypt...\n...decrypt...");
+	Serial.println(F("...encrypt...\n...decrypt..."));
 
 	// aes128.decryptBlock(test_input, test_buf);
 	// printArr(test_input);
@@ -69,14 +73,17 @@ void sendUpdate(IPAddress ip) {
 	Serial.println(buf);
 
 	memcpy(aesBuf, buf, sizeof(buf));
-	aes128.encryptBlock((uint8_t *)buf, aesBuf);
-	Serial.println(buf);
+	Serial.println((char *)aesBuf);
+	memset(aesBuf, 0, sizeof(aesBuf));
+	aes128.cbc_encrypt((uint8_t *)buf, (byte *)aesBuf, 16, iv);
+	Serial.write((char *)aesBuf, sizeof(aesBuf));
+	Serial.println();
 	
 	if (updClient.connect(updSrvAddr, srvPort)) {
 		if (updClient.connected())
-			Serial.println("connected");
+			Serial.println(F("connected"));
 
-		Serial.println("sending addr upd");
+		Serial.println(F("sending addr upd"));
 
 		updClient.println("PUT /api/spduAPI HTTP/1.1");
 		updClient.print("Host: ");
@@ -85,9 +92,15 @@ void sendUpdate(IPAddress ip) {
 		updClient.println("Connection: close");
 		updClient.println("Content-Type: application/x-www-form-urlencoded;");
 		updClient.print("Content-Length: ");
-		updClient.println(strlen(buf));
+		
+		Serial.print("aesBuf length - ");
+		Serial.println(sizeof(aesBuf));
+
+		updClient.println(sizeof(aesBuf));
 		updClient.println();
-		updClient.println(buf);
+		// updClient.println((char *)aesBuf);
+		updClient.write(aesBuf, sizeof(aesBuf));
+		// updClient.println();
 		delay(10);
 	} else {
 		Serial.println(F("could not connect to update server"));
@@ -109,26 +122,26 @@ void procSwitchReq(void)
 			if (client.available()) {
 				delay(5);
 				client.readBytes(buf, sizeof(buf));
-				client.find("Content-Length:");
-				client.find("\r\n\r\n");
-				client.find("\"name\":\"");
-				int outlet = client.parseInt();
-				client.find("\"state\":\"");
-				int state = client.parseInt();
+				// client.find("Content-Length:");
+				// client.find("\r\n\r\n");
+				// client.find("\"name\":\"");
+				// int outlet = client.parseInt();
+				// client.find("\"state\":\"");
+				// int state = client.parseInt();
 
 
-				sprintf(buf, "name - %d\nstate - %d", outlet, state);
-				Serial.println(buf);
+				// // sprintf(buf, "name - %d\nstate - %d", outlet, state);
+				// // Serial.println(buf);
 
 
-				delay(5);
-				client.println("HTTP/1.1 200 OK");
-				client.println("Content-Type: text/html");
-				client.println("Connection: close");
-				client.println();
+				// delay(5);
+				// client.println("HTTP/1.1 200 OK");
+				// client.println("Content-Type: text/html");
+				// client.println("Connection: close");
 				// client.println();
-				snprintf(buf, sizeof(buf), "Changed outlet #%d to \"%s\"", outlet, state ? "On" : "Off");
-				client.println(buf);
+				// // client.println();
+				// snprintf(buf, sizeof(buf), "Changed outlet #%d to \"%s\"", outlet, state ? "On" : "Off");
+				// client.println(buf);
 
 				delay(5);
 				client.stop();
@@ -194,8 +207,8 @@ void setup()
 	{
 		pinMode(relayPins[pin], OUTPUT); //pin selected to control
 		digitalWrite(relayPins[pin], outletStates[pin] == 1 ? HIGH : LOW);
-		sprintf(buf, "pin - %d, state - %d", relayPins[pin], outletStates[pin]);
-		Serial.println(buf);
+		// sprintf(buf, "pin - %d, state - %d", relayPins[pin], outletStates[pin]);
+		// Serial.println(buf);
 	}
 	//-------------------------------------------------------------------------------------------------------
 	//-------------------------------------------------------------------------------------------------------
