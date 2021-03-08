6e55d2682d7b5614f5128b93d8554ad35fbf991a
hy6p <roman.tata.b@gmail.com>
2021-03-08 02:17:53 -0800

    Refactor msg to shorter format

--- src/main.ino
+++ src/main.ino
@@ -17,12 +17,13 @@ static const char aesKey_out[] = "abcdefghijklmnop";
 // Init outlet relay pins and state
 //-------------------------------------------------------------------------------------------------------
 // String readString;
+int fail_count = 0;
 millisDelay updateDelay;
 int tmp = 0;
 char inputChar;
-char buf[170];
-char msg[170];
-byte iv[N_BLOCK];
+char buf[128];
+char msg[128];
+byte iv[N_BLOCK], ivInit[N_BLOCK];
 int *intBlock = (int *)iv;
 // byte aesBuf[256];
 int relayPins[] = {2, 3, 4, 6};
@@ -39,11 +40,27 @@ int srvPort = 7071;
 
 AES aes128;
 
+int printArr(void *arr, int length) {
+	if (arr == NULL) {
+		return -1;
+	}
+	int i = 0;
+	char *ptr = (char *)arr;
+
+	Serial.print(F("arr with sz - "));
+	Serial.println(length);
+	for (i = 0; i < length; i++) {
+		Serial.print(ptr[i]);
+	}
+	Serial.println();
+
+	return 0;
+}
 int getRandomBlock(byte *block) {
 	if (!block)
 		return -1;
 	delay(1);
-	memset(iv, 0, 17);
+	memset(iv, 0, 16); //setting 17 bytes overwrites msg
 	randomSeed(updateDelay.remaining());
 	for (int i = 0; i < 4; i++) {
 		intBlock[i] = random(RAND_MAX);
@@ -53,6 +70,8 @@ int getRandomBlock(byte *block) {
 		Serial.print((char)iv[i]);
 		if (iv[i] == 0)
 			iv[i] = 1;
+		if (iv[i] == '\\')
+			iv[i] = 1;
 	}
 	Serial.println();
 	return 0;
@@ -61,13 +80,23 @@ int getRandomBlock(byte *block) {
 int encrypt(byte *msg, int tlength) {
 	// const uint8_t *key = "abcdefghijklmnop";
 	// memcpy(iv, key, N_BLOCK);
-	getRandomBlock(iv);
+	getRandomBlock(ivInit);
+	memcpy(iv, ivInit, N_BLOCK);
 	aes128.clean();
-	aes128.set_key((byte *)aesKey_in, 16);
+	aes128.set_key((byte *)aesKey_out, 16);
 	Serial.println(F("...encrypt..."));
-	aes128.cbc_encrypt(msg, (byte *)buf, 16, iv);
-	Serial.write((char *)buf, tlength);
-	Serial.println();
+	Serial.print(F("message with length - "));
+	Serial.println(tlength);
+	Serial.write((char *)msg, tlength);
+	int n_blocks = tlength / 16;
+	Serial.print(F("message with nlocks - "));
+	Serial.println(n_blocks);
+	aes128.cbc_encrypt(msg, (byte *)buf, n_blocks, iv);
+	aes128.clean();
+	memcpy(iv, ivInit, N_BLOCK);
+	// printArr((void *)buf, tlength);
+	Serial.write((uint8_t *)buf, tlength);
+	// Serial.println();
 	return 0;
 }
 
@@ -76,10 +105,12 @@ int setMsg(byte *msg, IPAddress ip) {
 	memset(buf, 0, sizeof(buf));
 	getRandomBlock(iv);
 	// sprintf(buf, "{\"password\": \"secpass123\",\"name\": \"ard\",\"payload\": {\"deviceName\": \"arduino\",\"ipAddress\": \"%d.%d.%d.%d\",\"key\": \"%s\"}}", ip[0], ip[1], ip[2], ip[3], iv);
-	memset(buf, 0, sizeof(buf));
-	tmp = sprintf(buf, "{\"password\": \"secpass123\",\"name\": \"ard\",\"payload\": {\"deviceName\": \"arduino\",\"ipAddress\": \"%d.%d.%d.%d\",\"key\": \"", ip[0], ip[1], ip[2], ip[3]);
-	memcpy(buf + tmp, iv, 16);
-	memcpy_P(buf + tmp + 16, F("\"}}"), 3);
+	// memset(buf, 0, sizeof(buf));
+	tmp = sprintf(buf, "{\"password\": \"secpass123\",\"ipAddress\": \"%d.%d.%d.%d\",\"key\": \"", ip[0], ip[1], ip[2], ip[3]);
+	Serial.println(tmp);
+	memcpy(&buf[tmp], iv, 16);
+	tmp = tmp + sizeof(iv);
+	memcpy(&buf[tmp], "\"}", 2);
 	tmp = sizeof(buf) - 1;
 	while(tmp && !buf[tmp]) {
 		tmp--;
@@ -92,7 +123,7 @@ int setMsg(byte *msg, IPAddress ip) {
 	if (((ilength) % 16) == 0) {
 		Serial.print(F("message fits int number of blocks of 16 - "));
 		Serial.println(ilength);
-		msg = (byte*)malloc(ilength);
+		// msg = (byte*)malloc(ilength);
 	} else {
 		Serial.print(F("message will be padded to int number of blocks of 16 - "));
 		plength = ((ilength/16) + 1)*16 - ilength;
@@ -100,15 +131,18 @@ int setMsg(byte *msg, IPAddress ip) {
 		// msg = (byte*)malloc(ilength + plength);
 	}
 	tlength = ilength + plength;
-	if (!msg) {
-		Serial.println(F("Not enough memory.\n"));
-		return -1;
-	}
+	// if (!msg) {
+	// 	Serial.println(F("Not enough memory.\n"));
+	// 	return -1;
+	// }
 	Serial.println(tlength);
 	memset(msg, 0, tlength);
 	memcpy(msg, buf, tlength);
 	/*PKCS7 padding*/
+	Serial.print("PKCS7 padding - fill with ");
+	Serial.print(plength);
 	for (int end = ilength; end < tlength; end++) {
+		Serial.print(end);
 		msg[end] = plength;
 	}
 	// Serial.printf("n-2 = %x; n-1 = %x\n", buf[sizeof(buf) - 2], buf[sizeof(buf) - 1]);
@@ -118,7 +152,7 @@ int setMsg(byte *msg, IPAddress ip) {
 	return tlength;
 }
 
-void sendUpdate(IPAddress ip) {
+int sendUpdate(IPAddress ip) {
 	// webserver.reset();
 	// IPAddress *localip;
 	Serial.println(F("Start sendUpdate"));
@@ -127,9 +161,10 @@ void sendUpdate(IPAddress ip) {
 	int tlength = setMsg((byte *)msg, ip);
 	if (tlength <= 0) {
 		Serial.println(F("Error compositing update message."));
-		return;
+		return -1;
 	}
-
+	Serial.write(msg, tlength);
+	Serial.println("\nafter set before enc");
 	encrypt((byte *)msg, tlength);
 	
 	if (updClient.connect(updSrvAddr, srvPort)) {
@@ -139,7 +174,7 @@ void sendUpdate(IPAddress ip) {
 		updClient.flush();
 		if (!updClient.availableForWrite()) {
 			Serial.println(F("Client err."));
-			return;
+			return -1;
 		}
 		Serial.println(F("sending addr upd"));
 
@@ -162,7 +197,11 @@ void sendUpdate(IPAddress ip) {
 		// updClient.println();
 		delay(10);
 	} else {
+		if (fail_count > 2)
+			return -2;
+
 		Serial.println(F("could not connect to update server"));
+		fail_count++;
 	}
 	tmp = 0;
 	updClient.stop();
@@ -177,7 +216,7 @@ void sendUpdate(IPAddress ip) {
 	// Serial.write(msg, tlength);
 	// Serial.println();
 	// free(msg);
-	return;
+	return 0;
 }
 
 void procSwitchReq(void) 
@@ -242,6 +281,8 @@ void procSwitchReq(void)
 // 	Serial.print(F("\n"));
 // }
 
+void(* resetFunc)(void) = 0;
+
 void setup()
 {
 	//enable serial data print
@@ -249,6 +290,8 @@ void setup()
 	Serial.begin(9600);
 	while (!Serial); // wait for serial port
 	begin:
+	fail_count = 0;
+
 	// if (!SD.begin(4)) {
 	// 	Serial.println("Failed to initialize SD card.");
 	// 	return;
@@ -305,7 +348,12 @@ void loop()
 		swServer.flush();
 		// tmp = 1;
 		Serial.println(F("Delay"));
-		sendUpdate(Ethernet.localIP());
+		tmp = sendUpdate(Ethernet.localIP());
+		if (tmp == -2 && fail_count > 1) {
+			Serial.println("too many fails.. restart..");
+			resetFunc();
+		}
+		
 		updateDelay.start(15000);
 		swServer.begin();
 		// tmp = 0;
