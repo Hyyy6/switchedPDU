89150e1ad113069f0e8353a4d2f08f68dc0eccd7
rb <roman.e.babichev@gmail.com>
2023-04-07 19:15:22 -0700

    Simple rework: Complete Arduino-Server communication.
    TODO:
    Rework Arduino-Client  communication.
    Console/ethernet management?

--- src/main.ino
+++ src/main.ino
@@ -4,21 +4,10 @@
 #include "Ethernet.h"
 #include "millisDelay.h"
 #include <SPI.h>
-// #include "AES.h"
 #include "avr/wdt.h"
-// #include "base64.hpp"
 #include "utility/w5100.h"
-#include "ArduinoHttpClient.h"
-// #include "secrets.h"
-// #include "EEPROM.h"
-
-// byte aesKey_in_new[N_BLOCK];// = "abcdefghijklmnop";
-// byte aesKey_in_old[N_BLOCK];// = "abcdefghijklmnop";
-byte psk_srv = 'S';
-byte psk_client = 'C';
-byte rand_byte = 0;
-byte crypt = 0;
-// bool is_debug = true;
+byte psk_srv = 179;
+byte psk_client = 55;
 #define DEBUG
 
 #define START_MEM_ADDR 0
@@ -30,38 +19,31 @@ byte crypt = 0;
 int fail_count = 0;
 millisDelay updateDelay;
 int tmp = 0;
-// int tmp1 = 0;
-// char inputChar;
 char buf[80];
-// char msg[80];
-// char big_buf[300];
-// byte iv[N_BLOCK], ivInit[N_BLOCK];
 int relayPins[] = {6, 7, 8, 9};
 int outletStates[] = {1, 1, 1, 1};
-// static uint8_t mac[] = {0x2c,0xf0,0x5d,0x8a,0xda,0x89};
 static uint8_t mac[] = {0xF8, 0x62, 0x30, 0x4C, 0x7D, 0xC5};
-// {0xF8, 0x62, 0x30, 0x4C, 0x7D, 0xC5};
-// {0x2c,0xf0,0x5d,0x8a,0xda,0x89};
-// IPAddress gateway(192, 168, 1, 1);
-// IPAddress dns(8, 8, 8, 8);
 
 EthernetClient updClient;
 EthernetServer swServer(1234);
-// byte updSrvAddr[] = {192, 168, 0, 113};
-// IPAddress updSrvAddr(192, 168, 0, 113);
 #ifndef DEBUG
-static const char conn_string[] = "hosts.spduapi.azurewebsites.net";
+// static const char conn_string[] = "hosts.spduapi.azurewebsites.net";
 // static const char host[] = "spduapi.azurewebsites.net";
+#define conn_string "hosts.spduapi.azurewebsites.net"
+#define host "spduapi.azurewebsites.net"
+#define updDelay 6000 
 #else
-static const char conn_string[] = "10.10.0.156";
+// static const char conn_string[] = "10.10.0.156";
 // static const char host[] = "10.10.0.156:6969";
+#define conn_string "10.10.0.156"
+#define host "10.10.0.156:6969"
+#define updDelay 60000 
 #endif
-const char *conTarget;
+// const char *conTarget;
 
 int srvPort = 6969;
 // int srvPort = 7071;
-HttpClient httpClient(updClient, conn_string, srvPort);
-// AES aes128;
+
 
 
 int setOutletState(uint8_t outlet, uint8_t state) {
@@ -88,15 +70,10 @@ int updateFromLocal() {
 	for(int i = 0; i < OUTLET_COUNT; i++) {
 		addr = (byte *)START_MEM_ADDR + i;
 		value = eeprom_read_byte(addr);
-		// Serial.printf(F("value - "));
-		// Serial.println(value);
 		if (value > 1) {
 			value = 1;
 			eeprom_update_byte(addr, value);
 		}
-		
-		// Serial.printf(F("set outlet "));Serial.print(i);Serial.printf(F("to value "));Serial.println(value);
-		// outletStates[i] = value;
 		setOutletState(i, value);
 	}
 	
@@ -148,136 +125,26 @@ int printArr(void *arr, int length) {
 
 	return 0;
 }
-// int getRandomBlock(byte *block) {
-// 	if (!block)
-// 		return -1;
-	
-	
-// 	delay(1);
-// 	memset(block, 0, 16); //setting 17 bytes overwrites msg
-// 	for (int i = 0; i < 4; i++) {
-// 		tmp = random(RAND_MAX);
-// 		for (int j = 0; j < 4; j++) {
-// 			block[i*4 + j] = ((byte*)&tmp)[j];
-// 		}
-// 	}
-// 	Serial.print("random block of 16: ");
-// 	for (int i = 0; i < 16; i++) {
-// 		Serial.print(block[i]);
-// 		Serial.print(' ');
-// 		if (block[i] == 0)
-// 			block[i] = 1;
-// 		if (block[i] == '\\')
-// 			block[i] = 1;
-// 	}
-// 	Serial.println();
-// 	return 0;
-// }
-
-// int encrypt(byte *plain, byte *cipher, byte *key, byte *IV, int tlength) {
-// 	Serial.println(F("Encrypt..."));
-// 	int n_blocks;
-// 	if (!cipher || !plain || !key || !IV || tlength <= N_BLOCK) {
-// 		return -1;
-// 	}
-// 	// Serial.println("ivInit");
-// 	// getRandomBlock(ivInit);
-// 	// memcpy(iv, ivInit, N_BLOCK);
-// 	aes128.clean();
-// 	aes128.set_key(key, 16);
-// 	Serial.print(F("message with length - "));
-// 	Serial.println(tlength);
-// 	Serial.write((char *)plain, tlength);
-// 	n_blocks = tlength / 16;
-// 	Serial.print(F("message with nlocks - "));
-// 	Serial.println(n_blocks);
-// 	aes128.cbc_encrypt(plain, cipher, n_blocks, iv);
-// 	aes128.clean();
-// 	// memcpy(iv, ivInit, N_BLOCK);
-// 	Serial.write((uint8_t *)cipher, tlength);
-// 	return 0;
-// }
-
-// int decrypt(byte *cipher, byte *plain, byte *key, byte* IV, int tlength) {
-// 	Serial.println(F("Decrypt..."));
-// 	int n_blocks;
-
-// 	if (!cipher || !plain || !key || !IV || tlength <= N_BLOCK) {
-// 		return -1;
-// 	}
-// 	aes128.clean();
-
-// 	aes128.set_key(key, N_BLOCK);
-// 	n_blocks = tlength / N_BLOCK;
-
-// 	aes128.cbc_decrypt(cipher, plain, n_blocks, IV);
-// 	aes128.clean();
-// 	return 0;
-// }
 
 int setMsg(char *msg, int len, IPAddress ip) {
-	// int ilength = 0, plength = 0, tlength = 0;
-	// int b64_len = 0;
+	byte rand_byte = 0;
+	byte crypt;
 	memset(msg, 0, sizeof(len));
-	// memcpy(aesKey_in_old, aesKey_in_new, N_BLOCK);
-	// Serial.println("aesKey_in_new generate:");
-	// getRandomBlock(aesKey_in_new);
 	rand_byte = random(UINT8_MAX);
 	crypt = rand_byte ^ psk_srv;
-	tmp = sprintf(msg, "{\"key\": %d,\"ipAddress\": [%d,%d,%d,%d]\"", crypt, ip[0], ip[1], ip[2], ip[3]);
-	// memset(msg, 0, len);
-	// b64_len = encode_base64((char*)aesKey_in_new, 16, msg);
-	// printArr(msg, 24);
-	// Serial.print(F("length of base64 encoded key: "));
-	// Serial.println(b64_len);
-
-	// memcpy(&buf[tmp], msg, b64_len);
-	// tmp += sprintf(&buf[tmp], "")
-	// Serial.println((char*)aesKey_in_new);
-	// tmp = tmp + b64_len;
-	// Serial.printf("+++ size - %d +++\n%s\n", tmp, msg);
+	tmp = sprintf(msg, "{\"key\": %d,\"ipAddress\": [%d,%d,%d,%d]", crypt, ip[0], ip[1], ip[2], ip[3]);
 	sprintf(&msg[tmp], ",\"state\": [%d, %d, %d, %d]}", EXPAND_OUTLET_ARRAY(outletStates));
-	// Serial.printf("[%d, %d, %d, %d]\n", outletStates[0], outletStates[1], outletStates[2], outletStates[3]);
-	// Serial.printf("[%d, %d, %d, %d]\n", EXPAND_OUTLET_ARRAY(outletStates));
-	// memcpy(&buf[tmp], "\"}", 2);
-	// Serial.printf("added %d bytes\n", tmp1);
-	// tmp += tmp1;
-	// Serial.printf("+++ size - %d +++\n%s\n", tmp, msg);
 	tmp = len - 1;
 	while(tmp && !msg[tmp]) {
 		tmp--;
 	}
-	tmp += 1; //size of msg;
-	// ilength = tmp;
-	// Serial.print(F("size of message - "));
-	// Serial.println(tmp);
-	// if (((ilength) % 16) == 0) {
-	// 	// Serial.print(F("message fits int number of blocks of 16 - "));
-	// 	// Serial.println(ilength);
-	// } else {
-	// 	// Serial.print(F("message will be padded to int number of blocks of 16 - "));
-	// 	plength = ((ilength/16) + 1)*16 - ilength;
-	// }
-	// tlength = ilength + plength;
-	// Serial.println(tlength);
-	// memset(msg, 0, tlength);
-	// memcpy(msg, buf, tmp);
-	/*PKCS7 padding*/
-	// Serial.print(F("PKCS7 padding - fill with "));
-	// Serial.print(plength);
-	// Serial.println();
-	// for (int end = ilength; end < tlength; end++) {
-	// 	Serial.print(end);
-	// 	msg[end] = plength;
-	// }
-	// Serial.write(msg, tlength);
-	// Serial.println();
-	// Ethernet.MACAddress(mac);
-	// printArr(mac, 6);
+	tmp += 1;
 	return tmp;
 }
 
 int sendUpdate(IPAddress ip) {
+	int fail = 0;
+begin:
 	Serial.println(F("Start sendUpdate"));
 	int tlength = setMsg(buf, sizeof(buf), ip);
 	if (tlength <= 0) {
@@ -285,117 +152,74 @@ int sendUpdate(IPAddress ip) {
 		return -1;
 	}
 	Serial.printf("set msg, bytes - %d\n================\n%s\n", tlength, buf);
-	// Ethernet.MACAddress(mac);
-	// printArr(mac, 6);
-	// getRandomBlock(ivInit);
-	// memcpy(iv, ivInit, N_BLOCK);
-	// encrypt((byte *)msg, (byte*)buf, (byte*)aesKey_out, iv, tlength);
-	// memcpy(iv, i)
-	// updClient.flush();
-	// delay(100);
-	// tmp = 0;
-	// while (tmp < 10) {
-	// 		conTarget = (char *)conn_string;
-	// 		if (!updClient.connect(conTarget, srvPort)) {
-	// 			delay(100);
-	// 			tmp++;
-	// 			Serial.print(F("retry "));
-	// 			Serial.println(tmp);
-	// 		} else {
-	// 			break;
-	// 		}
-	// 	// conTarget = (byte *)conn_string;
-		
-	// }
-	// if (updClient.connected()) {
-		// memset(msg, 0, sizeof(msg));
-		// delay(100);
-		// Serial.println(F("\nconnected"));
-
-		// updClient.flush();
-		// if (!updClient.availableForWrite()) {
-		// 	Serial.println(F("Client err."));
-		// 	return -1;
-		// }
-		Serial.print(F("sending addr upd"));
-		// memset(big_buf, 0, sizeof(big_buf));
-		// String data = "GET /api/upd HTTP/1.1\r\n";
-		// // updClient.write(data.begin(), data.length());
-		// // data = "User-Agent: Arduino/1.0\r\n";
-		// data += "User-Agent: Arduino/1.0\r\n";
-		// // updClient.write(data.begin(), data.length());
-		// // data = "Accept: */*\r\n";
-		// data += "Accept: */*\r\n";
-		// // updClient.write(data.begin(), data.length());
-		// // data ="Host: ";//%s\n";;
-		// data +="Host: ";
-		// data += host;
-		// updClient.write(data.begin(), data.length());
-		// data = "\r\nConnection: close\r\n\r\n";
-		// // data += "\nConnection: close\r\n";
-		// updClient.write(data.begin(), data.length());
-		// 		data += "Content-Type: text/html; charset=utf-8;\n";
-		// 		data += "Content-Length: ";//%d\n\n", host, (tlength+1));
-		// 		data += tlength;
-		// 		data += "\n\n";
-		// tmp = snprintf(big_buf, sizeof(big_buf), "POST /api/upd HTTP/1.1\r\n"
-		// 		"User-Agent: Arduino/1.0\r\n"
-		// 		"Accept: */*\r\n"
-		// 		"Host: %s\r\n"
-		// 		"Connection: keep-alive\r\n"
-		// 		"Content-Type: text/html; charset=utf-8;\r\n"
-		// 		"Content-Length: %d\r\n\r\n", host, (tlength));
-		// updClient.println("GET /api/upd HTTP/1.1");
-		// updClient.print("Host: ");
-		// updClient.write(host);
-		// // updClient.println();
-		// updClient.println("\nUser-Agent: Arduino/1.0");
-		// updClient.println("Connection: close");
-		// updClient.println("Content-Type: application/x-www-form-urlencoded;");
-		// updClient.print("Content-Length: ");
-
-		// Serial.print(F("msg length - "));
-		// Serial.println(tlength);
-		httpClient.post("/api/upd", "text/html", buf);
-		// updClient.println(tlength);
-		// updClient.println("\n");
-		// Ethernet.MACAddress(mac);
-		// printArr(mac, 6);
-		// Serial.printf("+++ header length - %d +++\n%s\n", tmp, big_buf);
-		// tmp += snprintf(&big_buf[tmp], sizeof(big_buf) - tmp, "%s\0", buf);
-		// Serial.printf("+++ added bytes - %d\n===============\n%s +++\n%s\n", tmp1, buf, big_buf);
-		// updClient.write(data.c_str(), data.length());
-		// data = buf;/
-		// updClient.write(big_buf); 
-		// updClient.write(buf, tlength);
-		// updClient.write(ivInit, N_BLOCK);
-		// updClient.write(data.begin(), data.length());
-		// updClient.flush();
-		// delay(500);
-
-		Serial.println(F("server response"));
-		// char c;
-		// memset(big_buf, 0, sizeof(big_buf));
-		// while (updClient.available()) {
-		// 	c = updClient.read((byte *)big_buf, sizeof(big_buf));
-		// 	Serial.printf("read %d bytes", (int )c); Serial.print(big_buf);
-		// 	Serial.print(big_buf);
-		// }
-		int code = httpClient.responseStatusCode();
-		String response = httpClient.responseBody();
-		Serial.printf("code - %d, body: \n%s\n", code, response.c_str());
-		Serial.printf(F("end server response\n"));
-	// } else {
-	// 	if (fail_count > 2)
-	// 		return -2;
-
-	// 	Serial.printf(F("could not connect to update server\n"));
-	// 	Serial.printf("%d\n", fail_count);
-	// 	fail_count++;
-	// }
 	tmp = 0;
-	// updClient.flush();
-	// updClient.stop();
+	while (tmp < 10) {
+			// conTarget = (char *)conn_string;
+			if (!updClient.connect(conn_string, srvPort)) {
+				delay(100);
+				tmp++;
+				Serial.print(F("retry "));
+				Serial.println(tmp);
+			} else {
+				break;
+			}
+	}
+	if (updClient.connected()) {
+		Serial.println(F("\nconnected"));
+
+		updClient.flush();
+		if (!updClient.availableForWrite()) {
+			Serial.println(F("Client err."));
+			return -1;
+		}
+		Serial.print(F("sending addr upd"));
+		String data = "PUT /api/upd HTTP/1.1\r\n";
+		data += "User-Agent: Arduino/1.0\r\n";
+		data += "Accept: */*\r\n";
+		data +="Host: ";
+		data += host;
+		data += "\r\nConnection: close\r\n";
+				data += "Content-Type: text/html; charset=utf-8;\r\n";
+				data += "Content-Length: ";//%d\n\n", host, (tlength+1));
+				data += tlength;
+				data += "\r\n\r\n";
+		// updClient.write(data.begin(), data.length());
+
+		data += buf;
+		updClient.write(data.begin(), data.length());
+
+
+		char c;
+		tmp = 0;
+		data = "";
+		while (!updClient.available()) {/*Serial.print("delay");*/delay(1);}
+
+		while (updClient.available()) {
+			c = updClient.read();
+			// Serial.print(c);
+			// data += updClient.read();
+			data += c;
+			tmp++;
+		}
+		Serial.printf("server response, bytes read - %d, len - %d\n", tmp, data.length());
+		// for (tmp = 0; tmp < data.length(); tmp++) {
+			Serial.print(data);
+		// }
+		Serial.printf(F("end server response\n"));
+		if (!strstr(data.c_str(), "HTTP/1.1 200 OK") || !strstr(data.c_str(), "Success")) {
+			Serial.printf("Bad response. Retry #%d\n", fail);
+			delay(10000);
+			fail++;
+			goto begin;
+		} else {
+			Serial.println("Update sent.");
+		}
+	} else {
+
+	}
+	tmp = 0;
+	updClient.flush();
+	updClient.stop();
 	ShowSockStatus();
 	return 0;
 }
@@ -594,7 +418,7 @@ void loop()
 		Serial.print(F("ram available: "));
 		Serial.println(freeRam());
 		// swServer.flush();
-		Serial.println(F("Delay"));
+		// Serial.println(F("Delay"));
 		Ethernet.MACAddress(mac);
 		printArr(mac, 6);
 		tmp = sendUpdate(Ethernet.localIP());
@@ -604,7 +428,7 @@ void loop()
 			resetFunc();
 		}
 		
-		updateDelay.start(6000);
+		updateDelay.start(updDelay);
 		Serial.println(updateDelay.remaining());
 		// swServer.flush();
 	}
